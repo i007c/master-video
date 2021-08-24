bf49ee6a5a2c9776bd3385a625ba16d05caf3ba7
i007c <dr007cc@gmail.com>
2021-08-24 17:44:34 +0430

    add some dev stuffs

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// import video player
-import MasterVideo from '../src/index'
+// master video
+import MasterVideo, { Options } from '../src/'
+
+// video file
+import videoFile from './videos/1.mp4'
+
+import './style.scss'
+
+const PlayerOptions: Options = {
+    source: videoFile,
+    controls: true,
+    style: { className: 'xxx' },
+}
 
 const App = () => {
     return (
-        <MasterVideo VideoSource={''} />
+        <div style={{ width: '100vw', height: '100vh', padding: '100px' }}>
+            <MasterVideo options={PlayerOptions} />
+        </div>
     )
 }
 
 export default App
-
 
 ReactDOM.render(<App />, document.getElementById('root'))
