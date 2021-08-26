5613170a6e8b816ea35badaf60bed48a680b3aa5
i007c <dr007cc@gmail.com>
2021-08-27 00:42:27 +0430

    copy some style from some where random

--- src/player/components/Controls.tsx
+++ src/player/components/Controls.tsx
@@ -6,31 +6,58 @@ import './sass/controls.scss'
 // default icons
 import { Pause, Play, Volume } from './icons'
 
-type ControlsProps = {
+interface ControlsProps {
     video: RefObject<HTMLVideoElement>
+}
+
+interface DcTimeType {
+    duration: number
+    currentTime: number
 }
 
 const Controls = ({ video }: ControlsProps) => {
     const [isPlaying, setIsPlaying] = useState(false)
     const [volumeValue, setVolVal] = useState(0)
+    const [dcTime, setDcTime] = useState<DcTimeType>({
+        duration: 0,
+        currentTime: 0,
+    })
 
     useEffect(() => {
-        if (video.current) {
-            video.current.onended = (): void => {
-                setIsPlaying(false)
-            }
-            video.current.onvolumechange = () => {
-                if (!video.current) return;
+        if (!video.current) return
 
-                if (video.current.muted) {
-                    setVolVal(0)
-                } else {
-                    setVolVal(video.current.volume * 100)
-                }
-            }
-            
-            setVolVal(video.current.volume * 100)
+        video.current.oncanplay = () => {
+            if (!video.current) return
+
+            setDcTime({
+                duration: Math.floor(video.current.duration),
+                currentTime: Math.floor(video.current.currentTime),
+            })
         }
+
+        video.current.ontimeupdate = () => {
+            if (!video.current) return
+
+            setDcTime({
+                duration: Math.floor(video.current.duration),
+                currentTime: Math.floor(video.current.currentTime),
+            })
+        }
+
+        video.current.onended = (): void => {
+            setIsPlaying(false)
+        }
+        video.current.onvolumechange = () => {
+            if (!video.current) return
+
+            if (video.current.muted) {
+                setVolVal(0)
+            } else {
+                setVolVal(video.current.volume * 100)
+            }
+        }
+
+        setVolVal(video.current.volume * 100)
     }, [video])
 
     const TogglePlay = (): void => {
@@ -54,13 +81,41 @@ const Controls = ({ video }: ControlsProps) => {
         <div className='controls-container'>
             <div className='controls'>
                 <div
-                    className='icon play-pause-btn'
+                    className='controler-section icon play-pause'
                     onClick={() => TogglePlay()}
                 >
                     {isPlaying ? <Pause /> : <Play />}
                 </div>
-                <div className='icon volume-btn' onClick={() => Togglemute()}>
+                <div
+                    className='controler-section icon volume'
+                    onClick={() => Togglemute()}
+                >
                     <Volume percentage={volumeValue} />
+                    <div className='volume-control'>
+                        <span className='root'>
+                            <span className='rail'></span>
+                            <span
+                                className='track'
+                                style={{ left: 0, width: '23%' }}
+                            ></span>
+                            <span
+                                className='thumb'
+                                role='slider'
+                                style={{ left: '23%' }}
+                                data-index='0'
+                                aria-labelledby='continuous-slider'
+                                aria-orientation='horizontal'
+                                // ariaValuemax='100'
+                                // ariaValuemin='0'
+                                // ariaValuenow='23'
+                            ></span>
+                        </span>
+                    </div>
+                </div>
+                <div className='controler-section dc-time'>
+                    <span>
+                        {dcTime.currentTime} / {dcTime.duration}
+                    </span>
                 </div>
             </div>
         </div>
