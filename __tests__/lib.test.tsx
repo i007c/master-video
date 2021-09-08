2f3887bb13b10d4218b5e3f70c1501098b069185
i007c <dr007cc@gmail.com>
2021-09-08 18:15:17 +0430

    update tests

--- __tests__/lib.test.tsx
+++ __tests__/lib.test.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import MasterVideo from '../lib/';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import MasterVideo from '../lib/'
 
 import video from './videos/1.mp4'
 
 test('Rnder Master Video', () => {
-  render(<MasterVideo options={{ source: video, controls: true }} />);
-  const MasterElement = screen.getByRole('master-video')
-  expect(MasterElement).toBeInTheDocument();
-});
+    render(
+        <MasterVideo
+            options={{
+                source: [{ label: 'source 1', url: video }],
+                controls: true,
+            }}
+        />
+    )
+    const MasterElement = screen.getByRole('master-video')
+    expect(MasterElement).toBeInTheDocument()
+})
