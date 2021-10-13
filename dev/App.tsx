18cb9dfbb0d3bd8e49df1025bf77c1f2e23494be
i007c <dr007cc@gmail.com>
2021-10-13 13:39:32 +0330

    sync with version 2

--- dev/App.tsx
+++ dev/App.tsx
@@ -2,28 +2,68 @@ import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
 // master video
-import MasterVideo, { Options } from '../lib/'
+import MasterVideo, { OverlayProps, VideoOptions } from '../lib/'
 
 // video file
 import videoFile1 from './videos/1.mp4'
-const videoFile2 =
-    'https://cdn.discordapp.com/attachments/837976157609656323/842785794485780520/Shrek_1.mp4'
+// const videoFile2 =
+//     'https://cdn.discordapp.com/attachments/837976157609656323/842785794485780520/Shrek_1.mp4'
 
 // style
 import './style.scss'
 
 import favicon from './favicon.ico'
 
-const PlayerOptions: Options = {
+// const PlayerOptions: Options = {
+//     source: [
+//         { label: '1', url: videoFile1 },
+//         { label: '2', url: videoFile2 },
+//     ],
+//     controls: true,
+//     style: {
+//         className: 'custom-video-player',
+//         timelineColor: '#F00',
+//     },
+// }
+
+const TestOverlay = ({
+    TogglePlay,
+    Time,
+    ChangeVolume,
+    ToggleMute,
+    isPlaying,
+    isMuted,
+    volume,
+}: OverlayProps) => {
+    console.log(
+        `is playing: ${isPlaying}\nis muted: ${isMuted}\nVol: ${volume}`
+    )
+
+    return (
+        <div className='overlay' style={{ background: 'rgba(0,0,0,.7)' }}>
+            <button onClick={() => TogglePlay()}>Toggle Play</button>
+            <button onClick={() => ToggleMute()}>Toggle Mute</button>
+            <input
+                type='range'
+                onChange={e => ChangeVolume(+e.target.value)}
+                max='100'
+                min='0'
+                step='1'
+            />
+            <span>
+                <Time type='played' />
+            </span>
+        </div>
+    )
+}
+
+const Options: VideoOptions = {
     source: [
-        { label: '1', url: videoFile1 },
-        { label: '2', url: videoFile2 },
+        {
+            label: 'test',
+            url: videoFile1,
+        },
     ],
-    controls: true,
-    style: {
-        className: 'custom-video-player',
-        timelineColor: '#F00',
-    },
 }
 
 const App = () => {
@@ -35,7 +75,7 @@ const App = () => {
     }, [])
     return (
         <div className='app'>
-            <MasterVideo options={PlayerOptions} />
+            <MasterVideo Options={Options} Overlay={TestOverlay} />
         </div>
     )
 }
