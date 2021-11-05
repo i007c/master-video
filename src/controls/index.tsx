6f165f3379ae74df82dcce08416e78eac60750c6
i007c <dr007cc@gmail.com>
2021-11-05 22:51:20 +0330

    use new icons and add some functionality

--- src/controls/index.tsx
+++ src/controls/index.tsx
@@ -2,34 +2,82 @@ import React, { PureComponent, ReactElement } from 'react'
 
 import { PlayerContext } from '../context'
 
+// utils
+import { togglePlay, toggleFullScreen } from '../utils'
+import TimeConvert from '../utils/TimeConvert'
+
+// components
+import CTime from './CTime'
+import FullScreen from './icons/FullScreen'
+// icons
+import Play from './icons/Play'
+import Volume from './icons/Volume'
+
 import './sass/controls.scss'
 
 interface ControlsProps {}
 
-interface ControlsState {}
+interface ControlsState {
+    isPlaying: boolean
+    isFullScreen: boolean
+    isMuted: boolean
+    volume: number
+    duration: string
+}
+
+const defaultState: ControlsState = {
+    isPlaying: false,
+    isFullScreen: false,
+    isMuted: false,
+    volume: 1.0,
+    duration: '0:00',
+}
 
 export class Controls extends PureComponent<ControlsProps, ControlsState> {
-    override state: ControlsState = {}
+    override state = defaultState
     // context setup
     static override contextType = PlayerContext
     declare context: React.ContextType<typeof PlayerContext>
 
     private video = this.context.video
+    private master = this.context.master
 
     override componentDidMount() {
-        console.log(this.video)
+        this.video.addEventListener('canplay', () =>
+            this.setState({ duration: TimeConvert(this.video.duration) })
+        )
     }
 
     override render(): ReactElement {
         return (
             <div className='controls-container'>
+                <div
+                    className='toggle-play'
+                    onClick={() => togglePlay(this.video)}
+                ></div>
                 <div className='controls'>
-                    <section className='left'>
-                        <div className='play-btn'></div>
-                    </section>
-                    <section className='right'>
-                        <div className='fullscreen'></div>
-                    </section>
+                    <div
+                        className='btn play-pause'
+                        onClick={() => togglePlay(this.video)}
+                    >
+                        <Play />
+                    </div>
+                    <div className='timestamp'>
+                        <CTime /> / {this.state.duration}
+                    </div>
+                    <div className='timeline'></div>
+                    <div
+                        className='btn volume'
+                        onClick={() => (this.video.muted = !this.video.muted)}
+                    >
+                        <Volume />
+                    </div>
+                    <div
+                        className='btn fullscreen'
+                        onClick={() => toggleFullScreen(this.master)}
+                    >
+                        <FullScreen />
+                    </div>
                 </div>
             </div>
         )
--- src/utils/player.functions.ts
+++ src/utils/player.functions.ts
@@ -2,3 +2,13 @@ export function togglePlay(video: HTMLVideoElement) {
     if (video.paused) video.play()
     else video.pause()
 }
+
+export function toggleFullScreen(element: HTMLElement) {
+    if (!document.fullscreenEnabled) return
+
+    if (document.fullscreenElement === element) {
+        document.exitFullscreen()
+    } else {
+        element.requestFullscreen()
+    }
+}
