449b3fa389cfb392ec8f2573929c5f2b4dbbfdc1
i007c <dr007cc@gmail.com>
2021-11-05 21:41:29 +0330

    adding master element to the mix

--- src/context/index.ts
+++ src/context/index.ts
@@ -2,6 +2,7 @@ import { createContext } from 'react'
 
 export interface PlayerContextType {
     video: HTMLVideoElement
+    master: HTMLDivElement
 }
 
 export const PlayerContext = createContext<PlayerContextType>(
