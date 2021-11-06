f3cd8d2b1069f61c50836af414f085f7b72dc240
i007c <dr007cc@gmail.com>
2021-11-07 00:04:03 +0330

    change test file

--- __tests__/lib.test.tsx
+++ __tests__/lib.test.tsx
@@ -7,13 +7,9 @@ import video from './videos/1.mp4'
 test('Rnder Master Video', () => {
     render(
         <MasterVideo
+            source={video}
             options={{
-                source: [{ label: 'source 1', url: video }],
-                controls: true,
-                style: {
-                    className: 'test-class',
-                    timelineColor: 'black'
-                },
+                loop: true,
             }}
         />
     )
